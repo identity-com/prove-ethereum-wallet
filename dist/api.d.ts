b53bc0a338e10eb8c38bc1cb2127673ca3cc8275
Kevin Colgan <kevin@civic.com>
2021-11-18 08:08:12 +0100

    fix: remove dist and add gitignore

--- dist/api.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer';
-import { CreatePowoOptions, EthPowoMessage, VerifyPowoOptions } from './types';
-export declare const createPowo: (signTypedData: (domain: TypedDataDomain, types: Record<string, TypedDataField[]>, value: EthPowoMessage) => Promise<string>, { domain, types, verifierAddress }: CreatePowoOptions) => Promise<string>;
-export declare const verifyPowo: (address: string, proof: string, { domain, types, verifierAddress }: VerifyPowoOptions) => Promise<boolean>;
